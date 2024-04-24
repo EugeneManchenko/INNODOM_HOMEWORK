97c389fef005bc8d7f3c7a200697b74040afb9e9
Yauheni <142231022+EugeneManchenko@users.noreply.github.com>
2024-04-24 13:57:05 +0300

    Измененная структура css
    настройка html шаблонов

--- mynews/itblog/main/views.py
+++ mynews/itblog/main/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+
+
 def index(request):
-    return render(request, 'main/index.html', {'title': 'Главная страница'})
+    data = {
+        'title': 'Главная страница',
+        'values': ['somer', 'hello', '123']
+    }
+    return render(request, 'main/index.html', data)
+
 
 def about(request):
     return render(request, 'main/about.html')
